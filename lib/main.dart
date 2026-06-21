e3dafd27f6a3523e5dd91d9e8fae4ef85bd96672
Romany Raafat <romanyraafat2004@gmail.com>
2026-06-21 14:03:27 +0300

    feat: add NoteModel with Hive integration and AppConstants for notes storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_app/core/helpers/constants.dart';
+import 'package:to_do_app/notes_app/data/models/note_model.dart';
 import 'package:to_do_app/notes_app/notes_app.dart';
 
-void main()
-{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(NoteModelAdapter());
+
+  await Hive.openBox<NoteModel>(AppConstants.kNotesBox);
   runApp(NotesApp());
 }
--- lib/notes_app/data/models/note_model.dart
+++ lib/notes_app/data/models/note_model.dart
@@ -1,17 +1,35 @@
 import 'dart:ui';
+import 'package:hive/hive.dart';
 
-class NoteModel {
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0) 
+class NoteModel extends HiveObject {
+  
+  @HiveField(0)
   final String title;
-  final String subTitle;
-  final String date;
-  final int color;
 
-  NoteModel({required this.title, required this.subTitle, required this.date, required this.color});
+  @HiveField(1)
+  final String subTitle;
+
+  @HiveField(2)
+  final String date;
+
+  @HiveField(3)
+  final int color; 
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+
   static final List<Color> noteColors = [
-    Color(0xFFE1BEE7),
-    Color(0xFFFFF9C4),
-    Color(0xFFFFCCBC),
-    Color(0xFFC8E6C9),
-    Color(0xB3E5FCFF),
+    const Color(0xFFE1BEE7),
+    const Color(0xFFFFF9C4),
+    const Color(0xFFFFCCBC),
+    const Color(0xFFC8E6C9),
+    const Color(0xB3E5FCFF),
   ];
 }
